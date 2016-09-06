1c3d5694c801dacfc1cf30685166b8cef91f2c58
wxmimperio <wxmimperio@outlook.com>
2016-09-06 14:49:27 +0800

    添加select table

--- sqlite-java/src/main/java/com/wxmimperio/sqlite/select/SelectData.java
+++ sqlite-java/src/main/java/com/wxmimperio/sqlite/select/SelectData.java
@@ -1,23 +1,56 @@
 package com.wxmimperio.sqlite.select;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by weiximing.imperio on 2016/9/6.
  */
 public class SelectData {
 
-    public static void selectData(Connection connection, String sql) {
+    public static List<Map<String, Object>> selectData(Connection connection, String sql) {
         Statement stmt = null;
+
+        List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
         try {
             stmt = connection.createStatement();
             connection.setAutoCommit(false);
 
+            ResultSet rs = stmt.executeQuery(sql);
+
+            connection.commit();
+            connection.setAutoCommit(true);
+
+            while (rs.next()) {
+                Map<String, Object> result = new ConcurrentHashMap<String, Object>();
+                result.put("id", rs.getInt("id"));
+                result.put("topic", rs.getString("topic"));
+                result.put("partition", rs.getInt("partition"));
+                result.put("offset", rs.getLong("offset"));
+                result.put("timestamp", rs.getTimestamp("timestamp"));
+                resultList.add(result);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
+        return resultList;
     }
 }
---
+++ sqlite-java/src/test/java/com/wxmimperio/sqlite/select/SelectDataTest.java
@@ -0,0 +1,32 @@
+package com.wxmimperio.sqlite.select;
+
+import com.wxmimperio.sqlite.connection.SQLiteConnection;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by weiximing.imperio on 2016/9/6.
+ */
+public class SelectDataTest {
+
+    @Test
+    public void selectDataTest() {
+        SQLiteConnection sqLiteConnection = new SQLiteConnection();
+        Connection conn = sqLiteConnection.getConnection();
+
+        String sql = "SELECT * FROM kafka_delay;";
+
+        List<Map<String, Object>> rsList = SelectData.selectData(conn, sql);
+        for (Map<String, Object> result : rsList) {
+            System.out.println("id=" + (int) result.get("id"));
+            System.out.println("topic=" + result.get("topic"));
+            System.out.println("partition=" + (int) result.get("partition"));
+            System.out.println("offset=" + (long) result.get("offset"));
+            System.out.println("timestamp=" + result.get("timestamp"));
+            System.out.println();
+        }
+    }
+}
