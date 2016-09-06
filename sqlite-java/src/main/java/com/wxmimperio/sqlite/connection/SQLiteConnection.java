77e8a38dde83878efadf2d2b7eaa18d83d2564da
wxmimperio <wxmimperio@outlook.com>
2016-09-06 11:30:16 +0800

    SQLite Demo

---
+++ sqlite-java/src/main/java/com/wxmimperio/sqlite/connection/SQLiteConnection.java
@@ -0,0 +1,42 @@
+package com.wxmimperio.sqlite.connection;
+
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by weiximing.imperio on 2016/9/6.
+ */
+public class SQLiteConnection {
+
+    private final static Logger logger = Logger.getLogger(SQLiteConnection.class);
+    private ThreadLocal<Connection> threadLocal = new ThreadLocal<Connection>();
+    private static String propFile = System.getProperty("user.dir") + "/conf/";
+
+    public Connection getConnection() {
+        Connection conn = threadLocal.get();
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            if (conn == null || conn.isClosed()) {
+                try {
+                    Class.forName("org.sqlite.JDBC");
+                    conn = DriverManager.getConnection("jdbc:sqlite:" + propFile + "Database.db");
+                    threadLocal.set(conn);
+                } catch (Exception e) {
+                    logger.error("获取JDBC连接出错", e);
+                    throw e;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
