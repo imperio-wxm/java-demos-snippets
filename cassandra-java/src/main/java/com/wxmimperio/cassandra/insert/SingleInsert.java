91daffbac014fe0273ed86024865cb9b73bdea8e
wxmimperio <wxmimperio@outlook.com>
2016-09-07 16:04:47 +0800

    单条insert操作

---
+++ cassandra-java/src/main/java/com/wxmimperio/cassandra/insert/SingleInsert.java
@@ -0,0 +1,35 @@
+package com.wxmimperio.cassandra.insert;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.wxmimperio.cassandra.connection.CassandraConn;
+
+/**
+ * Created by weiximing.imperio on 2016/9/7.
+ */
+public class SingleInsert {
+
+    public static boolean insert(String sql) {
+        Cluster cluster = CassandraConn.getCluster();
+        boolean isExecute = false;
+
+        //获取session
+        Session session = null;
+
+        try {
+            session = cluster.connect();
+            session.execute(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            if (cluster != null) {
+                cluster.close();
+                isExecute = true;
+            }
+        }
+        return isExecute;
+    }
+}
---
+++ cassandra-java/src/test/java/com/wxmimperio/cassandra/insert/InsertTest.java
@@ -0,0 +1,22 @@
+package com.wxmimperio.cassandra.insert;
+
+import org.junit.Test;
+
+/**
+ * Created by weiximing.imperio on 2016/9/7.
+ */
+public class InsertTest {
+
+    @Test
+    public void singleInsert() {
+        String insertSQL = "INSERT INTO kafka_crass.kafka_table (id,topic,offset) " +
+                "VALUES (1,'topic_001',123456789);";
+        String json = "{\"id\":\"2\",\"topic\":\"topic_002\",\"offset\":\"9876543210\"}";
+        String jsonSQL = "INSERT INTO kafka_crass.kafka_table JSON '" + json + "'";
+
+        System.out.println(jsonSQL);
+
+        SingleInsert.insert(insertSQL);
+        SingleInsert.insert(jsonSQL);
+    }
+}
