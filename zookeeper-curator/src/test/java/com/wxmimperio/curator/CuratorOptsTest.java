c9ab0bf91d961af2c931f3d1ba810f602c6da98f
wxmimperio <wxmimperio@outlook.com>
2016-11-14 16:10:16 +0800

    节点操作

---
+++ zookeeper-curator/src/test/java/com/wxmimperio/curator/CuratorOptsTest.java
@@ -0,0 +1,40 @@
+package com.wxmimperio.curator;
+
+import com.wxmimperio.curator.opts.CuratorOpts;
+import org.junit.Test;
+
+/**
+ * Created by weiximing.imperio on 2016/11/14.
+ */
+public class CuratorOptsTest {
+
+    @Test
+    public void OptsTest() throws Exception {
+        String path = "/consumers/group_2";
+        CuratorOpts curatorOpts = new CuratorOpts();
+
+        //检查/consumers/group_1是否存在
+        System.out.println(curatorOpts.isPathExist(path));
+
+        //添加节点
+        curatorOpts.addNode(path, "test");
+
+        System.out.println(curatorOpts.isPathExist(path));
+
+        //读取节点信息
+        curatorOpts.readData(path);
+
+        //更新节点
+        curatorOpts.updateNode(path,"test_update");
+
+        //读取更新后信息
+        curatorOpts.readData(path);
+
+        //删除节点
+        curatorOpts.deleteNode(path);
+
+        //验证是否删除
+        System.out.println(curatorOpts.isPathExist(path));
+
+    }
+}
