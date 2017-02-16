ee213530056ebdfaa00b1e46500e7c20abdedb19
“wxmimperio <wxmimperio@outlook.com>
2017-02-16 18:56:11 +0800

    "update for multiple files"

--- file-md5-check/src/main/java/com/wxmimperio/file/md5/MainCheck.java
+++ file-md5-check/src/main/java/com/wxmimperio/file/md5/MainCheck.java
@@ -1,6 +1,9 @@
 package com.wxmimperio.file.md5;
 
+import com.wxmimperio.file.cli.OptionsUtils;
+
 import java.io.File;
+import java.util.Map;
 
 /**
  * Created by weiximing.imperio on 2017/2/15.
@@ -8,23 +11,36 @@ import java.io.File;
 public class MainCheck {
 
     public static void main(String args[]) {
-        /**
-         * args[0] file path1
-         * args[1] file path2
-         * 两个文件md5相同则表示文件内容一样
-         */
 
-        File file1 = new File(args[0]);
-        File file2 = new File(args[1]);
+        Map<String, String[]> pathMap = OptionsUtils.getOptions(args);
+        String[] inputPath = pathMap.get("inputPath");
+        String[] outputPath = pathMap.get("outputPath");
 
-        if (file1.exists() || file2.exists()) {
-            if (FileMD5.getFileMD5(file1).equals(FileMD5.getFileMD5(file2))) {
-                System.out.println("文件：" + file1.getPath() + " 和文件：" + file2.getPath() + " MD5相同");
-            } else {
-                System.out.println("文件：" + file1.getPath() + " 和文件：" + file2.getPath() + " MD5不相同");
-            }
+        if (pathMap.isEmpty()) {
+            System.out.println("超时，请重新运行");
         } else {
-            System.out.println("文件：" + file1.getPath() + " 或文件：" + file2.getPath() + " 不存在");
+            if (inputPath.length != outputPath.length) {
+                System.out.println("输入、输出文件路径个数不同");
+            } else if (inputPath.length == 0 || outputPath.length == 0) {
+                System.out.println("输入、输出文件路径为空");
+            } else {
+                for (String inPath : inputPath) {
+                    for (String outPath : outputPath) {
+                        File inFile = new File(inPath);
+                        File outFile = new File(outPath);
+
+                        if (inFile.exists() || outFile.exists()) {
+                            if (FileMD5.getFileMD5(inFile).equals(FileMD5.getFileMD5(outFile))) {
+                                System.out.println("文件：" + inFile.getPath() + " 和文件：" + outFile.getPath() + " MD5相同");
+                            } else {
+                                System.out.println("文件：" + inFile.getPath() + " 和文件：" + outFile.getPath() + " MD5不相同");
+                            }
+                        } else {
+                            System.out.println("文件：" + inFile.getPath() + " 或文件：" + outFile.getPath() + " 不存在");
+                        }
+                    }
+                }
+            }
         }
     }
 }
