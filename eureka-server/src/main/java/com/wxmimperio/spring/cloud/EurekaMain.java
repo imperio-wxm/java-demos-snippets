312d21d2953869734294edd66284c0404b20dd39
wxmimperio <wxmimperio@outlook.com>
2017-03-14 00:38:15 +0800

    "update server"

--- eureka-server/src/main/java/com/wxmimperio/spring/cloud/EurekaMain.java
+++ eureka-server/src/main/java/com/wxmimperio/spring/cloud/EurekaMain.java
@@ -2,11 +2,14 @@ package com.wxmimperio.spring.cloud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 /**
  * Created by weiximing.imperio on 2017/3/13.
  */
+@EnableDiscoveryClient
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaMain {
