275c7a58d114323bc06150ae4784551e4ead15d8
wxmimperio <wxmimperio@outlook.com>
2017-04-10 23:42:11 +0800

    "close stream"

--- read-csv/src/main/java/com/wxmimperio/csv/CSVMain.java
+++ read-csv/src/main/java/com/wxmimperio/csv/CSVMain.java
@@ -3,6 +3,7 @@ package com.wxmimperio.csv;
 import com.wxmimperio.csv.utils.CSVHelper;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -21,6 +22,13 @@ public class CSVMain {
 
         for (String[] line : csvFile) {
             System.out.println(Arrays.asList(line));
+        }
+
+        try {
+            is.close();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
