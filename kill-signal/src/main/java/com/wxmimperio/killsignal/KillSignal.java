8904cd50785168466654a6296a7ba32f3a237836
“wxmimperio <wxmimperio@outlook.com>
2017-04-12 16:02:16 +0800

    "catch kill signal"

---
+++ kill-signal/src/main/java/com/wxmimperio/killsignal/KillSignal.java
@@ -0,0 +1,48 @@
+package com.wxmimperio.killsignal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+/**
+ * Created by weiximing.imperio on 2017/4/12.
+ */
+public class KillSignal implements SignalHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(KillSignal.class);
+
+    public KillSignal() {
+    }
+
+    public static void main(String[] args){
+        KillSignal killSignal = new KillSignal();
+        // TERM（kill -15）、USR1（kill -10）、USR2（kill -12）、USR2（kill -9）
+        //Signal.handle(new Signal("USR1"), killSignal);
+        Signal.handle(new Signal("TERM"), killSignal);
+        Signal.handle(new Signal("USR2"), killSignal);
+        //Signal.handle(new Signal("KILL"), killSignal);
+        killSignal.sleepTime();
+    }
+
+    @Override
+    public void handle(Signal signal) {
+        LOG.info("Signal [" + signal.getName() + "] is received, stopServer soon...");
+        stopProject();
+        LOG.info("Stop successfully.");
+    }
+
+    private void sleepTime() {
+        try {
+            Thread.sleep(50000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 安全的关闭HttpServer监听服务
+     */
+    private void stopProject() {
+        LOG.info("Stop!");
+    }
+}
