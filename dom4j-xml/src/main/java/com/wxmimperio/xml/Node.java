d111eea05d8fd443ab9afffef43e12145fd299dd
wxmimperio <wxmimperio@outlook.com>
2017-04-20 00:35:17 +0800

    "finish analysis xml"

---
+++ dom4j-xml/src/main/java/com/wxmimperio/xml/Node.java
@@ -0,0 +1,35 @@
+package com.wxmimperio.xml;
+
+/**
+ * Created by wxmimperio on 2017/4/20.
+ */
+public class Node {
+
+    private String name;
+    private String type;
+    private String desc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
--- dom4j-xml/src/main/java/com/wxmimperio/xml/XmlMain.java
+++ dom4j-xml/src/main/java/com/wxmimperio/xml/XmlMain.java
@@ -1,12 +1,16 @@
 package com.wxmimperio.xml;
 
+import com.google.gson.*;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
+import javax.sound.midi.Soundbank;
 import java.io.File;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by wxmimperio on 2017/4/19.
@@ -16,19 +20,41 @@ public class XmlMain {
         String fileName = System.getProperty("user.dir") + "/src/main/resources/pt_web_js_log.xml";
         File inputXml = new File(fileName);
         SAXReader saxReader = new SAXReader();
+        JsonObject jsonObject = new JsonObject();
+
         try {
             Document document = saxReader.read(inputXml);
-            Element element = document.getRootElement();
-            for (Iterator i = element.elementIterator(); i.hasNext(); ) {
-                Element user = (Element) i.next();
-                for (Iterator j = user.elementIterator(); j.hasNext(); ) {
-                    Element node = (Element) j.next();
-                    System.out.println(node.getName() + ":" + node.getText());
+            Element rootElement = document.getRootElement();
+            for (Iterator i = rootElement.elementIterator(); i.hasNext(); ) {
+                Element firstElement = (Element) i.next();
+                jsonObject.addProperty("tableName", firstElement.attribute("name").getText());
+                jsonObject.addProperty("desc", firstElement.attribute("desc").getText());
+
+                JsonArray fields = new JsonArray();
+                for (Iterator j = firstElement.elementIterator(); j.hasNext(); ) {
+                    Element secondElement = (Element) j.next();
+                    Map<String, String> field = new ConcurrentHashMap<String, String>();
+                    field.put("name", secondElement.attribute("name").getText());
+                    field.put("type", secondElement.attribute("type").getText());
+                    field.put("desc", secondElement.attribute("desc").getText());
+                    fields.add(new JsonParser().parse(new Gson().toJson(field)));
                 }
-                System.out.println();
+                jsonObject.add("fields", fields.getAsJsonArray());
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+        System.out.println(jsonObject.get("tableName"));
+        System.out.println(jsonObject.get("desc"));
+
+        JsonArray resultFields = jsonObject.get("fields").getAsJsonArray();
+
+        Gson elementGson = new Gson();
+        for (JsonElement jsonElement : resultFields) {
+            System.out.println(elementGson.fromJson(jsonElement, Node.class).getName() + " " +
+                    elementGson.fromJson(jsonElement, Node.class).getType() + " " +
+                    elementGson.fromJson(jsonElement, Node.class).getDesc());
         }
     }
 }
