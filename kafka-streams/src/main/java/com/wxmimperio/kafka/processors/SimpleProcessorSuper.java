7a68becba78ce09ba5cd638c0c265d3c029fa58e
wxmimperio <wxmimperio@outlook.com>
2017-10-27 14:31:08 +0800

    add simple processor

---
+++ kafka-streams/src/main/java/com/wxmimperio/kafka/processors/SimpleProcessorSuper.java
@@ -0,0 +1,15 @@
+package com.wxmimperio.kafka.processors;
+
+import org.apache.kafka.streams.processor.Processor;
+import org.apache.kafka.streams.processor.ProcessorSupplier;
+
+/**
+ * Created by wxmimperio on 2017/6/22.
+ */
+public class SimpleProcessorSuper implements ProcessorSupplier {
+
+    @Override
+    public Processor get() {
+        return new SimpleProcessor();
+    }
+}
