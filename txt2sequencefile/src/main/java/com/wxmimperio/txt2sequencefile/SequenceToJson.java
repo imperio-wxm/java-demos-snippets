faf7c691f7b81783eb5ac434acd511513b6384cf
wxmimperio <wxmimperio@outlook.com>
2017-12-16 15:50:32 +0800

    add SequenceToJson

---
+++ txt2sequencefile/src/main/java/com/wxmimperio/txt2sequencefile/SequenceToJson.java
@@ -0,0 +1,16 @@
+package com.wxmimperio.txt2sequencefile;
+
+public class SequenceToJson {
+
+    public static void main(String[] args) {
+        String inputPath = args[0];
+        String outputPath = args[1];
+        String topic = args[2];
+
+        try {
+            HDFSRunner.sequenceFileReader2Json(inputPath, outputPath, topic);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
