208bf57965006362e3ff6b0906af7201e9f92c51
wxm <wxmimperio@outlook.com>
2018-03-14 22:52:57 +0800

    add 类::实例方法名

--- java8-demo/src/main/java/com/wxmimperio/java8/demo4/MethodReference.java
+++ java8-demo/src/main/java/com/wxmimperio/java8/demo4/MethodReference.java
@@ -3,6 +3,8 @@ package com.wxmimperio.java8.demo4;
 import com.wxmimperio.java8.pojo.Person;
 import org.junit.Test;
 
+import java.util.Comparator;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -28,5 +30,26 @@ public class MethodReference {
         Supplier<Integer> supplier1 = person::getAge;
         int age = supplier1.get();
         System.out.println(age);
+    }
+
+    // 类::静态方法名
+    @Test
+    public void reference3() {
+        Comparator<Integer> comparator = (x, y) -> Integer.compare(x, y);
+        System.out.println(comparator.compare(12, 53));
+
+        Comparator<Integer> comparator1 = Integer::compareTo;
+        System.out.println(comparator1.compare(56, 10));
+    }
+
+    // 类::实例方法名
+    @Test
+    public void reference4() {
+        // 当第一个参数为函数式接口的调用者，而第二个参数是函数式接口的参数时才能应用
+        BiPredicate<String, String> biPredicate = (x, y) -> x.equalsIgnoreCase(y);
+        System.out.println(biPredicate.test("123", "123"));
+
+        BiPredicate<String, String> biPredicate1 = String::equalsIgnoreCase;
+        System.out.println(biPredicate1.test("abc", "123"));
     }
 }
