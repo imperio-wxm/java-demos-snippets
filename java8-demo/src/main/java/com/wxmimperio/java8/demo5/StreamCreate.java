3d81b4a06cf8557189e6a7cb587995f4844fecb6
wxmimperio <wxmimperio@outlook.com>
2018-05-08 12:19:10 +0800

    add two list combine

--- java8-demo/src/main/java/com/wxmimperio/java8/demo5/StreamCreate.java
+++ java8-demo/src/main/java/com/wxmimperio/java8/demo5/StreamCreate.java
@@ -2,8 +2,10 @@ package com.wxmimperio.java8.demo5;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class StreamCreate {
@@ -27,5 +29,15 @@ public class StreamCreate {
 
         // 生成
         Stream.generate(() -> Math.random()).limit(10).forEach(System.out::println);
+    }
+
+    @Test
+    public void ops2() {
+        List<String> list1 = new ArrayList<>(Arrays.asList("1", "2", "3"));
+        System.out.println(list1);
+        List<String> list2 = new ArrayList<>(Arrays.asList("a", "b", "c"));
+        System.out.println(list2);
+
+        Stream.of(list1.stream(), list2.stream()).flatMap(Function.identity()).forEach(System.out::println);
     }
 }
