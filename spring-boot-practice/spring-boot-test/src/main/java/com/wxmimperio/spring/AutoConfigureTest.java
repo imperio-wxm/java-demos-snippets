59533076483d248279d3e20c2b5c724f5285ddb9
wxm <wxmimperio@outlook.com>
2018-06-09 23:13:20 +0800

    add test

---
+++ spring-boot-practice/spring-boot-test/src/main/java/com/wxmimperio/spring/AutoConfigureTest.java
@@ -0,0 +1,30 @@
+package com.wxmimperio.spring;
+
+import com.wxmimperio.spring.autoconfigure.service.AutoConfigureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class AutoConfigureTest {
+
+    private AutoConfigureService autoConfigureService;
+
+    @Autowired
+    public AutoConfigureTest(AutoConfigureService autoConfigureService) {
+        this.autoConfigureService = autoConfigureService;
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return autoConfigureService.getUserAndPass();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoConfigureTest.class);
+    }
+}
