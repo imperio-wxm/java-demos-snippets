2f2ba7b91bfee7da92f48e71ada4bf8ee1295536
wxmimperio <wxmimperio@outlook.com>
2018-06-11 17:15:01 +0800

    add redis-ops

---
+++ redis-ops/src/main/java/com/wxmimperio/redis/connection/RedisConnection.java
@@ -0,0 +1,44 @@
+package com.wxmimperio.redis.connection;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+public class RedisConnection {
+    private static final int DEFAULT_TIMEOUT = 2000;
+    private static final int DEFAULT_MAX_REDIRECTIONS = 5;
+    private JedisCluster redis = null;
+
+    private volatile static RedisConnection inst;
+
+    RedisConnection() {
+        ResourceBundle rb = ResourceBundle.getBundle("application", Locale.getDefault());
+        String url = rb.getString("redis.url");
+        String password = null;
+        Set<HostAndPort> nodes = new HashSet<>();
+        for (String from : url.split(",")) {
+            nodes.add(HostAndPort.parseString(from));
+        }
+        redis = new JedisCluster(nodes, DEFAULT_TIMEOUT, DEFAULT_TIMEOUT, DEFAULT_MAX_REDIRECTIONS, password, new JedisPoolConfig());
+    }
+
+    public static RedisConnection getInstance() {
+        if (inst == null) {
+            synchronized (RedisConnection.class) {
+                if (inst == null) {
+                    inst = new RedisConnection();
+                }
+            }
+        }
+        return inst;
+    }
+
+    public JedisCluster getRedis() {
+        return redis;
+    }
+}
---
+++ redis-ops/src/main/java/com/wxmimperio/redis/ops/MainOps.java
@@ -0,0 +1,40 @@
+package com.wxmimperio.redis.ops;
+
+import com.wxmimperio.redis.connection.RedisConnection;
+import redis.clients.jedis.JedisCluster;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MainOps {
+
+    private static final String REDIS_NAMESPACE_HBASE_ALIAS = "hbase_alias";
+
+    public static void main(String[] args) throws IOException {
+        JedisCluster jedisCluster = RedisConnection.getInstance().getRedis();
+
+        String alias = "test_table";
+        String table = alias + "_" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMdd_HHmmss"));
+        String key = REDIS_NAMESPACE_HBASE_ALIAS + ":" + alias;
+        //set(jedisCluster, key, table);
+
+        if (checkExists(jedisCluster, key)) {
+            System.out.println(get(jedisCluster, key));
+        }
+
+        jedisCluster.close();
+    }
+
+    public static boolean checkExists(JedisCluster jedisCluster, String key) {
+        return jedisCluster.exists(key);
+    }
+
+    public static void set(JedisCluster jedisCluster, String alias, String table) {
+        jedisCluster.set(alias, table);
+    }
+
+    public static String get(JedisCluster jedisCluster, String alias) {
+        return jedisCluster.get(alias);
+    }
+}
