7076c4b01af4a07a359e2fd84aa5ef86ef47bc59
wxm <wxmimperio@outlook.com>
2019-01-27 01:12:53 +0800

    add WebSecurityConfigurer 注册用户

---
+++ springcloud-learning/spring-oauth2/src/main/java/com/wxmimperio/springcloud/config/WebSecurityConfigurer.java
@@ -0,0 +1,34 @@
+package com.wxmimperio.springcloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 定义两个用户，一个角色为USER，一个角色为ADMIN
+        auth.inMemoryAuthentication() // 支持内存、jdbc、ldpa存储
+                .withUser("wxmimperio")
+                .password("wxmimperio")
+                .roles("USER")
+                .and()
+                .withUser("wxm")
+                .password("wxm")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+}
