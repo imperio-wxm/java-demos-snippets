126dd48ecac30c552dabc8bb889403020854893c
wxm <wxmimperio@outlook.com>
2019-01-29 01:53:54 +0800

    add service and controller

---
+++ springcloud-learning/springcloud-stream-kafka/src/main/java/com/wxmimperio/springcloud/controller/SendMessageController.java
@@ -0,0 +1,26 @@
+package com.wxmimperio.springcloud.controller;
+
+import com.wxmimperio.springcloud.service.SendMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class SendMessageController {
+
+    private SendMessageService sendMessageService;
+
+    @Autowired
+    public SendMessageController(SendMessageService sendMessageService) {
+        this.sendMessageService = sendMessageService;
+    }
+
+    @PostMapping("send/{actionName}")
+    public void sendMessage(@PathVariable String actionName) {
+        sendMessageService.sendMessage(actionName, UUID.randomUUID().toString());
+    }
+}
