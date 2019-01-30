f5e7e2f3c7028fdd3097ed07b80a17bc2cc3947b
wxmimperio <wxmimperio@outlook.com>
2019-01-30 19:01:09 +0800

    update client

--- springcloud-learning/eureka-client/src/main/java/com/wxmimperio/springcloud/controller/ClientController.java
+++ springcloud-learning/eureka-client/src/main/java/com/wxmimperio/springcloud/controller/ClientController.java
@@ -1,9 +1,22 @@
 package com.wxmimperio.springcloud.controller;
 
+import feign.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ClientController {
@@ -16,5 +29,23 @@ public class ClientController {
         String services = "Services: " + discoveryClient.getServices();
         System.out.println(services);
         return services;
+    }
+
+    /*@GetMapping("read/{fileName}")
+    public ResponseEntity<InputStreamResource> readFile(@PathVariable("fileName") String fileName) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(fileName.getBytes());
+        InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(out.toByteArray()));
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).contentLength(out.toByteArray().length).body(resource);
+    }*/
+
+    @GetMapping("read/{fileName}")
+    public ResponseEntity<byte[]> readFile(@PathVariable("fileName") String fileName, HttpServletResponse response) throws IOException {
+       /* Map<String, Collection<String>> headers = new HashMap<>();
+        Response response1 = feign.Response.builder().body(fileName, Charset.forName("UTF-8")).status(200).headers(headers).build();*/
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(fileName.getBytes());
+        response.getOutputStream().write(fileName.getBytes());
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).contentLength(out.toByteArray().length).body(out.toByteArray());
     }
 }
