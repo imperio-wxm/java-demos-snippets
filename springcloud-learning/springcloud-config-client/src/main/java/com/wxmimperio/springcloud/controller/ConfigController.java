6a0cfc31e3cd30bfb5d11780b657f17b2495d0e3
wxmimperio <wxmimperio@outlook.com>
2019-01-31 13:25:42 +0800

    update

--- springcloud-learning/springcloud-config-client/src/main/java/com/wxmimperio/springcloud/controller/ConfigController.java
+++ springcloud-learning/springcloud-config-client/src/main/java/com/wxmimperio/springcloud/controller/ConfigController.java
@@ -1,10 +1,12 @@
 package com.wxmimperio.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigController {
 
     @Value("${name}")
