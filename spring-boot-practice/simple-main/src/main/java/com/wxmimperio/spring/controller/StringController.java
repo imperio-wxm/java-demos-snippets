6bb50c6ea466f69574533cc6fe7a44fab21a7119
wxmimperio <wxmimperio@outlook.com>
2019-02-19 16:13:11 +0800

    update

--- spring-boot-practice/simple-main/src/main/java/com/wxmimperio/spring/controller/StringController.java
+++ spring-boot-practice/simple-main/src/main/java/com/wxmimperio/spring/controller/StringController.java
@@ -1,13 +1,21 @@
 package com.wxmimperio.spring.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.wxmimperio.spring.beans.DefaultTimeBean;
 import com.wxmimperio.spring.beans.TimeBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.UUID;
 
 @RestController
@@ -22,8 +30,21 @@ public class StringController {
         return "wxmimperio";
     }
 
-    @PostMapping("/time")
-    public TimeBean postDateTime(@RequestBody TimeBean timeBean) throws Exception {
+    @PostMapping("/formatJava8Time")
+    public TimeBean postDateTime(@RequestBody TimeBean timeBean) {
+        System.out.println(timeBean.getLocalDate());
+        System.out.println(timeBean.getLocalDateTime());
+        System.out.println(timeBean.getLocalTime());
         return timeBean;
+    }
+
+    @GetMapping("/getFormatJava8Time")
+    public TimeBean getFormatJava8Time() {
+        return new TimeBean(LocalDate.now(), LocalDateTime.now(), LocalTime.now());
+    }
+
+    @GetMapping("/getDefaultJava8Time")
+    public DefaultTimeBean getDefaultJava8Time() {
+        return new DefaultTimeBean(LocalDate.now(), LocalDateTime.now(), LocalTime.now());
     }
 }
