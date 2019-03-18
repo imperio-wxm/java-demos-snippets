d43ac669d82fc9673204060bc6e60e3872057e73
wxmimperio <wxmimperio@outlook.com>
2019-03-18 18:00:54 +0800

    add col info

--- spring-boot-practice/springboot-oracle/src/main/java/com/wxmimperio/spring/beans/Schema.java
+++ spring-boot-practice/springboot-oracle/src/main/java/com/wxmimperio/spring/beans/Schema.java
@@ -2,11 +2,14 @@ package com.wxmimperio.spring.beans;
 
 import com.wxmimperio.spring.common.SchemaType;
 
+import java.util.List;
+
 public class Schema {
 
     private String name;
     private SchemaType schemaType;
     private String comment;
+    private List<Column> columns;
 
     public Schema() {
     }
@@ -41,12 +44,21 @@ public class Schema {
         this.comment = comment;
     }
 
+    public List<Column> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<Column> columns) {
+        this.columns = columns;
+    }
+
     @Override
     public String toString() {
         return "Schema{" +
                 "name='" + name + '\'' +
                 ", schemaType=" + schemaType +
                 ", comment='" + comment + '\'' +
+                ", columns=" + columns +
                 '}';
     }
 }
--- spring-boot-practice/springboot-oracle/src/main/java/com/wxmimperio/spring/service/OracleService.java
+++ spring-boot-practice/springboot-oracle/src/main/java/com/wxmimperio/spring/service/OracleService.java
@@ -1,7 +1,9 @@
 package com.wxmimperio.spring.service;
 
 import com.google.common.collect.Lists;
+import com.wxmimperio.spring.beans.Column;
 import com.wxmimperio.spring.beans.Schema;
+import com.wxmimperio.spring.common.DataType;
 import com.wxmimperio.spring.common.SchemaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,7 @@ import java.util.List;
 public class OracleService {
 
     private DataSource phoenixDataSource;
+    private static final List<String> filter = Lists.newArrayList("SYS_EXPORT_SCHEMA_01");
 
     @Autowired
     public OracleService(DataSource phoenixDataSource) {
@@ -25,28 +28,80 @@ public class OracleService {
     }
 
     public List<Schema> getAllTables() {
-        List<Schema> tablesName = Lists.newArrayList();
+        List<Schema> tables = Lists.newArrayList();
         try (Connection connection = phoenixDataSource.getConnection();
-             Statement statement = connection.createStatement()) {
+             Statement statement = connection.createStatement();
+             Statement colStatement = connection.createStatement()) {
             String sql = "select t1.TABLE_NAME,t2.TABLE_TYPE,T2.COMMENTS from \n" +
                     "(select table_name TABLE_NAME from user_tables\n" +
                     "union all\n" +
                     "select view_name from user_views) t1 left join user_tab_comments t2 on t1.TABLE_NAME = t2.TABLE_NAME";
 
             ResultSet resultSet = statement.executeQuery(sql);
+
+            int count = 0;
+
             while (resultSet.next()) {
                 String tableName = resultSet.getString("TABLE_NAME");
-                String tableType = resultSet.getString("TABLE_TYPE");
-                String tableComment = resultSet.getString("COMMENTS");
-                tablesName.add(new Schema(
-                        tableName,
-                        StringUtils.isEmpty(tableType) ? SchemaType.TABLE : SchemaType.valueOf(tableType.toUpperCase()),
-                        StringUtils.isEmpty(tableComment) ? tableName : tableComment
-                ));
+
+                if (!filter.contains(tableName)) {
+                    String tableType = resultSet.getString("TABLE_TYPE");
+                    String tableComment = resultSet.getString("COMMENTS");
+
+                    Schema schema = new Schema(
+                            tableName,
+                            StringUtils.isEmpty(tableType) ? SchemaType.TABLE : SchemaType.valueOf(tableType.toUpperCase()),
+                            StringUtils.isEmpty(tableComment) ? tableName : tableComment
+                    );
+
+                    String colSql = "SELECT b.TABLE_NAME,b.column_name,b.data_type,b.data_precision,b.data_scale,b.column_id,b.char_length,a.comments FROM  USER_TAB_COLUMNS b,USER_COL_COMMENTS a WHERE b.TABLE_NAME = '" + tableName + "' AND b.TABLE_NAME = a.TABLE_NAME AND b.COLUMN_NAME = a.COLUMN_NAME";
+
+                    ResultSet colRs = colStatement.executeQuery(colSql);
+                    List<Column> columns = Lists.newArrayList();
+                    while (colRs.next()) {
+                        String colName = colRs.getString("COLUMN_NAME");
+
+
+                        String dataType = colRs.getString("DATA_TYPE");
+                        int dataPrecision = colRs.getInt("DATA_PRECISION");
+                        int dataScale = colRs.getInt("DATA_SCALE");
+                        int charLength = colRs.getInt("CHAR_LENGTH");
+                        String comment = colRs.getString("COMMENTS");
+
+                        DataType realDataType = null;
+                        try {
+                            for (DataType dt : DataType.values()) {
+                                if (dataType.startsWith(dt.name())) {
+                                    realDataType = dt;
+                                    break;
+                                }
+                            }
+
+                            if (realDataType == null) {
+                                System.exit(1);
+                            }
+
+                            if (realDataType.equals(DataType.NUMBER)) {
+                                if (dataPrecision == 0 && dataScale == 0) {
+                                    realDataType = DataType.INTEGER;
+                                }
+                            }
+                        } catch (IllegalArgumentException e) {
+                            e.printStackTrace();
+                            System.out.println("error table = " + tableName);
+                        }
+                        columns.add(new Column(colName, realDataType, comment));
+                    }
+                    schema.setColumns(columns);
+                    tables.add(schema);
+                    count++;
+                    System.out.println("table count = " + count);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return tablesName;
+        return tables;
     }
+
 }
