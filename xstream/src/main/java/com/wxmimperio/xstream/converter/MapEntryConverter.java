cafb36fb74df53300bb0e12cb98873de40a645dc
wxmimperio <wxmimperio@outlook.com>
2020-09-07 11:33:36 +0800

    init

---
+++ xstream/src/main/java/com/wxmimperio/xstream/converter/MapEntryConverter.java
@@ -0,0 +1,46 @@
+package com.wxmimperio.xstream.converter;
+
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author weiximing
+ * @version 1.0.0
+ * @className MapEntryConverter.java
+ * @description This is the description of MapEntryConverter.java
+ * @createTime 2020-09-07 11:17:00
+ */
+public class MapEntryConverter implements Converter {
+    public boolean canConvert(Class clazz) {
+        return AbstractMap.class.isAssignableFrom(clazz);
+    }
+
+    public void marshal(Object value, HierarchicalStreamWriter writer, MarshallingContext context) {
+        AbstractMap map = (AbstractMap) value;
+        for (Object obj : map.entrySet()) {
+            Map.Entry entry = (Map.Entry) obj;
+            writer.startNode(entry.getKey().toString());
+            writer.setValue(entry.getValue().toString());
+            writer.endNode();
+        }
+    }
+
+    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+        Map<String, String> map = new HashMap<>();
+        while (reader.hasMoreChildren()) {
+            reader.moveDown();
+            map.put(reader.getNodeName(), reader.getValue());
+            reader.moveUp();
+        }
+        return map;
+    }
+}
