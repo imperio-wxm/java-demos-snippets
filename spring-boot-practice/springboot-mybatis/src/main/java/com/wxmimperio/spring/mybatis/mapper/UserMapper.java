a9fa6a9c8ca86f589874a637222205d4492e3673
wxm <wxmimperio@outlook.com>
2021-07-08 02:09:53 +0800

    mybatis

---
+++ spring-boot-practice/springboot-mybatis/src/main/java/com/wxmimperio/spring/mybatis/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.wxmimperio.spring.mybatis.mapper;
+
+import com.wxmimperio.spring.mybatis.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
