5454229a39917cb5551b19de6e7c2d6af34736c1
wxm <wxmimperio@outlook.com>
2022-12-24 23:06:39 +0800

    update: 项目初始化

---
+++ jdbc-dynamic-datasource/src/test/java/com/wxmimperio/datasource/AppTest.java
@@ -0,0 +1,20 @@
+package com.wxmimperio.datasource;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Application.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
